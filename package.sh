1e27c49abb51530da4e5a3e61edd967968dd80d2
Naba Das <nabakr.das@Nabas-Mac-mini.local>
2023-04-26 16:07:33 +0530

    Extensions add fix

---
+++ package.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+word=$(echo -n "$INSTALL_ADDITIONAL_EXTENSIONS" | wc -w)
+for (( n=1; n<=$word; n++ ))
+# for n in ${word}
+do
+SERVER_ARRAY=`echo ${INSTALL_ADDITIONAL_EXTENSIONS} | awk '{print $'$n'}' | sed 's/.*-//'`
+sudo apk add --no-cache php${PHP_VERSION}-`echo ${SERVER_ARRAY}`
+done
